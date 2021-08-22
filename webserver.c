00f17e06ec3303421c5e480ca99b9c105fcc9c94
Luick Klippel <luickklippel@MacBook.local>
2021-08-22 12:49:02 +0200

    adding req parsing

--- webserver.c
+++ webserver.c
@@ -2,14 +2,17 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #include <unistd.h>
 
 #define MAX_CLIENTS 10
 
+#define CRLF 0x0D0A
+#define SP 0x0A
+
 typedef struct {
   unsigned short port;
-  char buf[1024];
   struct sockaddr_in server;
   int wserverSocket;
   pthread_t clientThreads[MAX_CLIENTS];
@@ -22,17 +25,44 @@ struct pthreadClientHandleArgs{
   int socket;
 };
 
+enum httpMethod {
+  GET,
+  POST
+};
+
+struct httpRequest {
+  int reqMethod;
+  double httpVersion;
+  char requestUri[];
+
+  // char *browserAccept;
+  // char *browserUserAgent;
+  // char *browserAccEncoding;
+  // char *accLang;
+  // char *connType;
+};
+
+void parseHttpRequest(char requestBuffer[], struct httpRequest *req) {
+  char *tok;
+  const char crlf = (char)CRLF;
+  char *buffCpy = strdup(requestBuffer);
+
+  while ((tok = strsep(&buffCpy, &crlf)) != NULL) {
+    printf("Line: %s \n", tok);
+  }
+}
+
 int wsInit(webserver *wserver, int port) {
   wserver->port = port;
 
   if ((wserver->wserverSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
-    printf("socket err");
+    printf("socket err \n");
     return 1;
   }
 
   wserver->server.sin_family = AF_INET;
-  wserver->server.sin_port   = htons(port);
+  wserver->server.sin_port = htons(port);
   wserver->server.sin_addr.s_addr = INADDR_ANY;
 
   wserver->mutexLock = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
@@ -40,22 +70,29 @@ int wsInit(webserver *wserver, int port) {
 
   if (bind(wserver->wserverSocket, (struct sockaddr *)&wserver->server, sizeof(wserver->server)) < 0)
   {
-    printf("bind err");
+    printf("bind err \n");
     return 1;
   }
 
   if (listen(wserver->wserverSocket, 1) != 0)
   {
-    printf("listenerr");
+    printf("listenerr \n");
     return 1;
   }
   return 0;
 }
 
-void *clientHandle(void *arg) {
-  printf("new client thread init sleep 1 \n");
-  sleep(1);
-  printf("sleep 1 passed exit \n");
+void *clientHandle(void *args) {
+  struct httpRequest *httpReq = (struct httpRequest*)malloc(sizeof(struct httpRequest));
+  struct pthreadClientHandleArgs *argss = (struct pthreadClientHandleArgs*)args;
+  char buff[1024] = {};
+
+  read(argss->socket, buff, sizeof(buff));
+
+  printf("From client: %s \n ", buff);
+  printf("----------------------------- \n");
+  parseHttpRequest(buff, httpReq);
+
   pthread_exit(NULL);
 }
 
@@ -91,7 +128,7 @@ int main() {
 
   rc = wsInit(wserver, 80);
   if (rc != 0) {
-    printf("failed to init struct \n");
+    printf("failed to init ws struct \n");
     return 1;
   }
 
@@ -101,7 +138,7 @@ int main() {
     return 1;
   }
 
-
-  printf("Server ended successfully\n");
+  free(wserver);
+  printf("Server ended successfully \n");
   return 0;
 }
